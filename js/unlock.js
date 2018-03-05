c88f24d4f12c59ba08aac9887c90787ffbbf40f6
Sam Andreae <sandreae@gmail.com>
2018-03-05 19:08:33 +0100

    unlock webaudio ios

--- js/unlock.js
+++ js/unlock.js
@@ -2,18 +2,23 @@ var isUnlocked = false;
 
 function unlock() {
 
+			
 
+	if(this.isUnlocked){
+		return;
+	} else {
+
+	console.log("unlock webaudio")
 	// create empty buffer and play it
 
-	var AudioContext = window.AudioContext || window.webkitAudioContext;
-	var myContext = new AudioContext();
+	var myContext = cracked.audioContext
 
 	var buffer = myContext.createBuffer(1, 1, 22050);
-	console.log(buffer)
+
 	var source = myContext.createBufferSource();
-	console.log(source)
+
 	source.buffer = buffer;
-	console.log(myContext.destination)
+
 	source.connect(myContext.destination);
 
 	source.start(0);
@@ -30,8 +35,6 @@ function unlock() {
 
 		}
 
-	}, 0);
-
-
-
+	}, 0);	
+	}
 }
